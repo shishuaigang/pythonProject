3612f86b3a66185d327e8264ac02c08e7ff73433
shisg <shisg@2345.com>
2020-10-28 21:44:18 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: list) -> int:
+        p, q = 0, len(height) - 1
+        max_area = 0
+        while p < q:
+            max_area = max(max_area, min(height[p], height[q]) * (q - p))
+            if height[p] < height[q]:
+                p += 1
+            else:
+                q -= 1
+        return max_area
+
+
+print(Solution().maxArea([1, 1]))
